2199cef244b0474ee6134159a1b7001245a32318
AyishaHub <121819240+AyishaHub@users.noreply.github.com>
2023-06-10 15:39:24 +0400

    Update myscripts.js
    replaced for loop

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,10 @@
 window.addEventListener('load',()=>{
     const navList=document.querySelectorAll("#nav__list li");
     const emptiness=document.querySelector(".empty_area");
-  for(let i=0;i<navList.length;i++){
-    navList[i].onclick=function(){
-        document.getElementById("txt").value=this.innerText;
-    };
-  };
+  navList.forEach(i=>{
+      i.addEventListener('click',()=>{
+          document.getElementById("txt").value=i.innerText;
+      
+    });
+  });
     });
