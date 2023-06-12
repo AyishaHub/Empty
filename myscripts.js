cb36b415cd60523726a9f781f846958fb30b57b1
AyishaHub <121819240+AyishaHub@users.noreply.github.com>
2023-06-12 15:49:18 +0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,9 @@
-window.addEventListener('load',()=>{
-    const navList=document.querySelectorAll("#nav__list li");
-    const emptiness=document.querySelector(".empty_area");
-  navList.forEach(i=>{
-      i.addEventListener('click',()=>{
-          document.getElementById("txt").value=i.innerText;
-      
+window.addEventListener('load', () => {
+  const navList = document.querySelectorAll('#nav__list li');
+
+  navList.forEach((i) => {
+    i.addEventListener('click', () => {
+      document.getElementById('txt').value = i.innerText;
     });
   });
-    });
+});
